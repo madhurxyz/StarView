3e27fe00726aa989e17248b4ba9c3fcc1eaa2aea
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-04 23:46:58 -0600

    added StarView.swift file

---
+++ StarView/StarView/StarView.swift
@@ -0,0 +1,35 @@
+//
+//  StarView.swift
+//  StarView
+//
+//  Created by Madhur Malhotra on 1/4/17.
+//  Copyright © 2017 madhur.xyz. All rights reserved.
+//
+
+import UIKit
+
+class StarView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
