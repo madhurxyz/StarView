ae76eb930c84143af6a9d0844690b64915274278
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-07 23:26:52 -0800

    added newStarView as a subview of self.view in viewController

--- StarView/StarView/ViewController.swift
+++ StarView/StarView/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(newStarView)
         // Do any additional setup after loading the view, typically from a nib.
         
         
